f1f3b62623352822f25839a1479b8ee5c4c355e1
Derrick Nyasio <dnyasio2014@gmail.com>
2022-08-19 16:03:44 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+//var bestCustomer = "";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "not bob";
+}
